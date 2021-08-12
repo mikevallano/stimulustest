6d588eb578f4086e592ac0d3c458e32a8343c64d
mike vallano <mikevallano@gmail.com>
2021-08-12 05:55:07 -0500

    Rename app

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Template61
+module Stimulustest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
