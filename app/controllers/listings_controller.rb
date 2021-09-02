ee5d1bfc25295a9404be6159051a693998856cb2
mike vallano <mikevallano@gmail.com>
2021-09-01 19:48:23 -0500

    Add create and delete listing functionality

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,21 @@
 class ListingsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @listing = Listing.new(listing_params.merge(creator_id: current_user.id))
+    @listing.save
+    redirect_to movie_path(@listing.movie)
   end
 
   def destroy
+    @listing = Listing.find_by(id: params[:id])
+    @listing.destroy
+    redirect_to movie_path(@listing.movie)
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:list_id, :movie_id, :creator_id)
   end
 end
